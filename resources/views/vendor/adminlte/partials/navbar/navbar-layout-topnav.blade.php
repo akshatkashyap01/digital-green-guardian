1d4f4e01c14d2dd770967dd545948f836ddd3c9e
akshat kashyap <akshatkashyap01@gmail.com>
2025-05-01 09:37:41 +0530

    made changes in navbar

--- resources/views/vendor/adminlte/partials/navbar/navbar-layout-topnav.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/navbar-layout-topnav.blade.php
@@ -1,59 +1,59 @@
 @inject('layoutHelper', 'JeroenNoten\LaravelAdminLte\Helpers\LayoutHelper')
 
-<nav class="main-header navbar
-    {{ config('adminlte.classes_topnav_nav', 'navbar-expand-md') }}
-    {{ config('adminlte.classes_topnav', 'navbar-white navbar-light') }}">
+<nav class="main-header navbar navbar-expand-md navbar-white navbar-light">
+    <div class="container">
 
-    <div class="{{ config('adminlte.classes_topnav_container', 'container') }}">
-
-        {{-- Navbar brand logo --}}
-        @if(config('adminlte.logo_img_xl'))
-            @include('adminlte::partials.common.brand-logo-xl')
-        @else
-            @include('adminlte::partials.common.brand-logo-xs')
-        @endif
-
-        {{-- Navbar toggler button --}}
-        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        {{-- Left: brand + toggle --}}
+        <a href="{{ url('/') }}" class="navbar-brand">
+            @if(config('adminlte.logo_img_xl'))
+                @include('adminlte::partials.common.brand-logo-xl')
+            @else
+                @include('adminlte::partials.common.brand-logo-xs')
+            @endif
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarCollapse" aria-controls="navbarCollapse"
+                aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        {{-- Navbar collapsible menu --}}
-        <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-            {{-- Navbar left links --}}
-            <ul class="nav navbar-nav">
-                {{-- Configured left links --}}
-                @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-left'), 'item')
+        {{-- Centered text --}}
+        <ul class="navbar-nav mx-auto d-none d-md-flex">
+            <li class="nav-item navbar-center-item">
+                <span class="nav-link font-weight-bold">
+                    Welcome to Admin Panel
+                </span>
+            </li>
+        </ul>
 
-                {{-- Custom left links --}}
-                @yield('content_top_nav_left')
+        {{-- Right: collapseable menu + user menu --}}
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+                @yield('content_top_nav_right')
+                @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-right'), 'item')
+                @if(Auth::check())
+                    @if(config('adminlte.usermenu_enabled'))
+                        @include('adminlte::partials.navbar.menu-item-dropdown-user-menu')
+                    @else
+                        @include('adminlte::partials.navbar.menu-item-logout-link')
+                    @endif
+                @endif
+                @if($layoutHelper->isRightSidebarEnabled())
+                    @include('adminlte::partials.navbar.menu-item-right-sidebar-toggler')
+                @endif
             </ul>
         </div>
 
-        {{-- Navbar right links --}}
-        <ul class="navbar-nav ml-auto order-1 order-md-3 navbar-no-expand">
-            {{-- Custom right links --}}
-            @yield('content_top_nav_right')
-
-            {{-- Configured right links --}}
-            @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-right'), 'item')
-
-            {{-- User menu link --}}
-            @if(Auth::user())
-                @if(config('adminlte.usermenu_enabled'))
-                    @include('adminlte::partials.navbar.menu-item-dropdown-user-menu')
-                @else
-                    @include('adminlte::partials.navbar.menu-item-logout-link')
-                @endif
-            @endif
-
-            {{-- Right sidebar toggler link --}}
-            @if($layoutHelper->isRightSidebarEnabled())
-                @include('adminlte::partials.navbar.menu-item-right-sidebar-toggler')
-            @endif
-        </ul>
-
     </div>
-
 </nav>
+@section('css')
+<style>
+    /* Ensure the .mx-auto list really centers itself */
+.navbar .navbar-center-item {
+  position: absolute;     /* pull it out of the standard flow */
+  left: 50%;              /* push its left edge to 50% */
+  transform: translateX(-50%); /* nudge it back by half its own width */
+}
+
+</style>
+@endsection
